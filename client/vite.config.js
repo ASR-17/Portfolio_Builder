8ca2225dfaea48c7941b643c0fcb702c19c3e4fc
ADITYA SINGH <adityasingh80026@gmail.com>
2025-06-11 18:13:45 +0530

    first commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio_Builder/', // Base path for GitHub Pages deployment
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // Allows @/ to point to /src
